f675b4803a760dd6290fcd79db96c791025e0494
exequielcabrera98 <execabrerasarrio@gmail.com>
2023-08-31 22:45:13 -0300

    Sistema de Gestión de Empleados

--- src/Gestion_de_Empleados/Main.java
+++ src/Gestion_de_Empleados/Main.java
@@ -5,28 +5,19 @@ public class Main {
     public static void main(String[] args) {
         EmployeeManager gestor = new EmployeeManager();
         Scanner scanner = new Scanner(System.in);
-        int iniciar, opcion, id;
+        int iniciar, opcion;
 
         do{
             System.out.println("¿Qué operación desea realizar?");
             System.out.println("1- Agregar empleado\n2- Eliminar empleado\n3- Editar empleado\n4- Listar empleados\n");
             opcion = scanner.nextInt();
 
-            switch (opcion){
-                case 1:
-                    gestor.addEmployee();
-                    break;
-                case 2:
-                    gestor.removeEmployee();
-                    break;
-                case 3:
-                    gestor.editEmployee();
-                    break;
-                case 4:
-                    gestor.viewEmployees();
-                    break;
-                default:
-                    System.out.println("La opción ingresada no es válida.");
+            switch (opcion) {
+                case 1 -> gestor.addEmployee();
+                case 2 -> gestor.removeEmployee();
+                case 3 -> gestor.editEmployee();
+                case 4 -> gestor.viewEmployees();
+                default -> System.out.println("La opción ingresada no es válida.");
             }
 
             System.out.println("¿Realizar otra operación?\n1- Si\n2- No");
